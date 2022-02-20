8080eb85fe83b92c581aa3d9cfff9e181d7d821a
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-02-20 11:16:51 +0000

    can check for years

--- sourceParser.js
+++ sourceParser.js
@@ -1,6 +1,6 @@
 const NUMBERS = ["0","1","2","3","4","5","6","7","8","9"];
 const UNITS = ["百", "千", "万", "億", "兆"];
-const DATES = ["月"];
+const DATES = ["月", "明治", "大正", "昭和", "平成", "令和"];
 const ORDINAL = ["第"]
 
 class SourceParser {
@@ -17,16 +17,35 @@ class SourceParser {
   }
 
   extractNumericalInformation = () => {
+    console.log(this.rawNumbers)
     for (const number of this.rawNumbers) {
       let indexOfNumber = this.rawString.indexOf(number);
-      let numericalInformation = this.rawString.substr(indexOfNumber-1, 3);
+      let indexBefore;
+      if (indexOfNumber === 0) {
+        indexBefore = 0;
+      } else if (indexOfNumber === 1) {
+        indexBefore = 1;
+      } else {
+        indexBefore = 2;
+      }
+      let textBeforeNumber = this.rawString.substr(indexOfNumber - indexBefore, indexBefore);
+      let textAfterNumber = this.rawString.substr(indexOfNumber+1, 1);
+      let numericalInformation = [textBeforeNumber, number, textAfterNumber];
       this.rawString = this.rawString.replace(number, "")
-      numericalInformation = numericalInformation.split("");
-      numericalInformation = numericalInformation.filter(character => this.relevantNumericalInfo.includes(character)).join("");
+      numericalInformation = numericalInformation.map(character => this.getRelevantNumber(character)).join("");
       this.processedNumbers.push(numericalInformation);
     }
-
     return this.processedNumbers;
+  }
+
+  getRelevantNumber = (substring) => {
+    let result = "";
+    for (const numericalInfo of this.relevantNumericalInfo) {
+      if (substring.includes(numericalInfo)) {
+        result = numericalInfo;
+      }
+    }
+    return result;
   }
 
 }
--- sourceParser.test.js
+++ sourceParser.test.js
@@ -12,4 +12,16 @@ describe("Checker", () => {
     expect(sourceParser.extractNumericalInformation()).toEqual(["第1", "1億"]);
   });
 
-})
+  it("should be able to extract Japanese years", () => {
+    let sourceParserYears = new SourceParser("今年は令和4年です");
+    sourceParserYears.extractNumbers()
+    expect(sourceParserYears.extractNumericalInformation()).toEqual(["令和4"]);
+  });
+
+  it("should function correctly if a number is at the end of the string", () => {
+    let sourceParserEnd = new SourceParser("安全第1");
+    sourceParserEnd.extractNumbers();
+    expect(sourceParserEnd.extractNumericalInformation()).toEqual(["第1"]);
+  })
+
+});
