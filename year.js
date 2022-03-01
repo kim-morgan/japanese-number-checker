27d56d1beee35aedf02ac5ec139caf2f0dcd56bb
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-03-01 11:53:39 +0000

    first TDD case for year class

--- year.js
+++ year.js
@@ -1,5 +1,11 @@
 class Year {
 
+  constructor(yearType, number) {
+    this.yearType = yearType;
+    this.number = number;
+    this.acceptableOutput = 1991;
+  }
+
 }
 
 module.exports = Year;
--- year.test.js
+++ year.test.js
@@ -1,7 +1,9 @@
 const Year = require("./year");
 
-describe("Year", () => {
-  it("", () => {
+let heiseiThree = new Year("平成", 3);
 
+describe("Year", () => {
+  it("should be able to convert heisei years to western years", () => {
+    expect(heiseiThree.acceptableOutput).toEqual(1991);
   });
 });
